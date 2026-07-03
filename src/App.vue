b65178622cb2eb12ffe2d1b5ed856a9fbecbe4ce
1872285531 <1872285531@qq.com>
2026-07-04 02:17:53 +0800

    feat: 组装 v3 视图切换 + 底部导航 + 弹窗，v3 完整功能就绪

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
-  <Desktop />
+  <div class="h-full overflow-y-auto">
+    <HomeView v-if="nav.currentView === 'home'" />
+    <PortfolioView v-else-if="nav.currentView === 'portfolio'" />
+    <AboutView v-else-if="nav.currentView === 'about'" />
+
+    <BottomNav />
+    <AppModal />
+  </div>
 </template>
 
 <script setup>
-import Desktop from './components/Desktop.vue'
+import { useNavigation } from './stores/navigation.js'
+import HomeView from './views/HomeView.vue'
+import PortfolioView from './views/PortfolioView.vue'
+import AboutView from './views/AboutView.vue'
+import BottomNav from './components/BottomNav.vue'
+import AppModal from './components/AppModal.vue'
+
+const nav = useNavigation()
 </script>
